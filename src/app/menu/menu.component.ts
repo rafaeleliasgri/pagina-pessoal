532fc730361fb85b68e2eca6d676928d6b5091f7
Rafael Elias Grigolo <rafaeleliasgri@alumni.usp.br>
2024-03-07 14:33:34 -0300

    menu do topo

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import { MenuService } from '../services/menu.service';
+import { Menu } from '../models/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
 
   constructor(private menuService: MenuService) { }
+
+  menuItems?: Array<Menu>;
+
+  menuStatus = 'closed';
+
+  ngOnInit() {
+    const menu = this.menuService.getItems();
+
+    this.menuItems = menu;
+  }
+
+  menuClick() {
+
+    this.menuStatus = (this.menuStatus === 'open' ? 'closed' : 'open');
+
+  }
 
 }
